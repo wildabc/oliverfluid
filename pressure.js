71b222e1191cae7caf50be4376b98928c07f467c
wildabc <a2903583@rmqkr.net>
2013-01-19 13:28:50 +0800

    don't use MacCormack by default

--- pressure.js
+++ pressure.js
@@ -340,8 +340,8 @@ function FluidField(canvas) {
         project(u, v, u0, v0);
         var temp = u0; u0 = u; u = temp; 
         var temp = v0; v0 = v; v = temp;
-        advectMacCormack(1, u, u0, u0, v0, dt);
-        advectMacCormack(2, v, v0, u0, v0, dt);
+        advect(1, u, u0, u0, v0, dt);
+        advect(2, v, v0, u0, v0, dt);
         project(u, v, u0, v0 );
     }
     var uiCallback = function(d,u,v) {};
